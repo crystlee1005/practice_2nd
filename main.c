2f77285392f710193394f4e45156b2b0ad7c4ae5
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-18 08:39:57 +0900

    003이론_변수사이즈

--- main.c
+++ main.c
@@ -4,19 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 // 3주차 이론   
-  //변수_예제  
+  //변수_사이즈 알아보기   
     
 
-int main(void)
+int main()
 {
-   float radius, area; // 변수 선언  
-   
-   radius = 10.0; // 상수 - 반지름  선언  
-   
-   area = 3.141592 * radius * radius; // 상수 - 원의 넓이 선언  
-   
-   printf("원의 면적 : %f\n", area); // 원의 넓이를 출력  
-   
-   return 0; 
+  int x;
+  printf("bytes of int : %i \n", sizeof(int));
+
+  
+  
+  return 0; 
+  
+
+  
     
  } 
+ 
