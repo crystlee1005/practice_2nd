6b52997ae5d0dc92a6e5908c7d0e5cd580095c8b
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-17 22:44:09 +0900

    002실습_4

--- main.c
+++ main.c
@@ -4,12 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 // 2주차 실습  
-  //C 프로그램 작성  
+  //연습 코딩   
     
 
 int main(void)
 {
-    printf("Hello, Sookmyung Electronics! \n"); // 글자를 출력  
+   int a, b; // 정수 변수 a,b 의 선언
+   a = 10;
+   b = 20;
+    
+   printf("%d + %d = %d \n", a, b, a+b); // 두 수의 합을  출력  
     
     return 0;
     
