e72749751d690efaff659a7b007c4865f7019fe6
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-17 22:35:41 +0900

    002이론
    덧셈프로그램

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+// 2주차 이론
+  //C 프로그래밍 구성 - 덧셈 프로그램  
+    
+	 /* 두개의 숫자의 합을 계산하는 프로그램 */ 
+int main(void)
+{
+	int x;
+	int y;
+	int sum;
+	
+	x = 100;
+	y = 200;
+	sum = x + y;
+	
+    printf("두수의 합 : %d\n", sum);
+    
+    return 0;
+    
+ } 
