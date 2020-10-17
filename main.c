6705eab67bf9c7ffaff74b1513c32a584b3e1d76
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-17 22:41:42 +0900

    002실습_2

--- main.c
+++ main.c
@@ -3,21 +3,13 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-// 2주차 이론
-  //C 프로그래밍 구성 - 덧셈 프로그램  
+// 2주차 실습  
+  //C 프로그램 작성  
     
-	 /* 두개의 숫자의 합을 계산하는 프로그램 */ 
+
 int main(void)
 {
-	int x; //더하기 첫번째 숫자 
-	int y; //더하기 두번째 숫자 
-	int sum; 
-	
-	x = 100;
-	y = 200;
-	sum = x + y;
-	
-    printf("두수의 합 : %d\n", sum); // 두수의 합을 출력 
+    printf("Hello, Sookmyung Electronics! \n"); // 글자를 출력  
     
     return 0;
     
