6edf3274c18e4b25ecb015ea4446b16730749eb3
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-18 11:13:21 +0900

    005예제-comtinue

--- mmain.c
+++ mmain.c
@@ -2,22 +2,19 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//5주차 예제- 순환문 for  문  
+//5주차 예제-continue   
 
  
 int main (void)
 {
-   int i, j;
+   int i;
    
-   for(i=0; i<5; i++)
+   for(i=0; i<10; i++)
    {
-   	for(j=0; j<10; j++)
-   	{
-   		printf("*");
-	}
-	printf("\n");
+   	if (i%3 == 0)
+   	continue; //조건문이 성립하면 다음문장을 실행하지 않고 증감 
+   	printf("%i", i); 
    }
-   
 	
 	 	
 }
