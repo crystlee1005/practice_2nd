7d045f567db852d556ceeede9c01d1e58e451b03
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-18 12:18:12 +0900

    005실습6

--- mmain.c
+++ mmain.c
@@ -2,33 +2,37 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//5주차 실습 5- 산술계산기     
+//5주차 실습 6- 숫자맞추기 게임      
 
  
 int main (void)
 {
-    int a, b; // 두 정수 변수 
-    char c;  
-    printf(" 식을 입력하세요: ");
-    scanf(" %i %c %i", &a, &c, &b); //숫자, 연산자, 숫자 입력  
+
+	int num, ans, trial;
+	ans=999;
+	trial=0;
 	
-	if(c== '+') //입력된 문자가 덧셈일때  
-	printf("= %i \n", a+b ); 
 	
-    else if(c=='-') // 입력된 문자가 뺄셈일때  
-	printf("= %i \n", a-b );
+     do{
+
+     printf("guess a number: ");
+      scanf("%i", &num);
+
+	 if(num<ans)
+	 printf("high!\n"); // 입력된 숫자가 정답보다 작으면  
+
+	 else if(num> ans)
+	 printf("low!\n"); // 입력된 숫자가 정답보다 크면 
+
+	 trial++; // 반복될  때마다 시도 횟수를 더한다. 
+
+
+	} while (num !=ans); // 입력된 숫자가 정답이 아닐 경우 do를 반복  
 	
-	else if(c=='*') // 입력된 문자가 곱셈일 때  
-	printf("= %i \n", a*b);
+	printf(" congratulations!  the number of trial: %i", trial ); // 정답이면 시도횟수와 함께 출력  
 	
-	else if(c=='/') //입력된 문자가 나눗셈일 때  
-	printf("= %i \n", a/b); 
-	
-	else if(c=='%') //입력된 문자가 나머지일 때  
-	printf("= %i \n", a%b);
-	
-return 0; 
-  
+	return 0;
+     
    }
 	
 	 	
