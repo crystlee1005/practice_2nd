ede5361f3706e235fd821d6eae1088d401a125a1
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-18 11:17:25 +0900

    005실습1

--- mmain.c
+++ mmain.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//5주차 예제-continue   
+//5주차 실습 1-양수 음수 판단   
 
  
 int main (void)
 {
    int i;
+   printf(" 정수 하나를 입력하시오: ");
+   scanf("%i", &i);
+    
+   if(i>0)
+   printf("양수입니다.");
    
-   for(i=0; i<10; i++)
-   {
-   	if (i%3 == 0)
-   	continue; //조건문이 성립하면 다음문장을 실행하지 않고 증감 
-   	printf("%i", i); 
+   else if (i<0)
+   printf("음수입니다.");
+   
+   else 
+   printf("0입니다."); 
    }
 	
 	 	
-}
