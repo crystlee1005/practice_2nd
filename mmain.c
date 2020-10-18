e8a1b71d4c9ca661c63de2c4e76cd22b6d29a478
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-18 09:58:24 +0900

    004실습4

--- mmain.c
+++ mmain.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//4주차 실습3, 분초를 계산  
+//4주차 실습4, 윤년을 계산  
  
 int main(int argc, char *argv[]) {
 	
