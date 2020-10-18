ce2d2bfe14773e6d357be9f807eb59bc2a4f62c9
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-18 09:57:51 +0900

    004실습4

--- mmain.c
+++ mmain.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//4주차 실습3
+//4주차 실습3, 분초를 계산  
  
 int main(int argc, char *argv[]) {
 	
-	int m, s;
+	int year, result;
 	
-	printf("input the second : ");
-	scanf("%i", &s);
+	printf("input the year : ");
+	scanf("%i", &year);
 	
-	m = s/60;
-    s = s%60;
-  printf("The time is %i : %i \n", m, s);
+	result = ((year%4 == 0)&&(year%100 != 0))||(year%400 == 0);
+  printf("is the year %i leap year? %i \n", year, result);
   
 
 	return 0;
