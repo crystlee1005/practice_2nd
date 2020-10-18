133a6b1bfc4f93fdf4a41794d98d8e2bc872fdc4
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-18 09:53:16 +0900

    004실습3

--- mmain.c
+++ mmain.c
@@ -2,20 +2,19 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//4주차 실습
+//4주차 실습3
  
 int main(int argc, char *argv[]) {
 	
-	int a, b;
+	int m, s;
 	
-	printf("input two integers : ");
-	scanf("%i %i", &a, &b);
+	printf("input the second : ");
+	scanf("%i", &s);
 	
-  printf("+ result is %i \n", (a+b));
-  printf("- result is %i \n", (a-b));
-  printf("* result is %i \n", (a*b));
-  printf("/ result is %i \n", (a/b));
-  printf("%% result is %i \n", (a%b));
+	m = s/60;
+    s = s%60;
+  printf("The time is %i : %i \n", m, s);
+  
 
 	return 0;
 }
