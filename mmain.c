b45b50d5fdbfc210bd04c3baa3bddcdb1606cc27
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-18 11:53:19 +0900

    005실습5

--- mmain.c
+++ mmain.c
@@ -2,24 +2,32 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//5주차 실습 4- 덧셈 프로그램   
+//5주차 실습 5- 산술계산기     
 
  
 int main (void)
 {
-    int number, sum, i;
-    sum=0;  //더하기 결과를 저장하는 변수 , 초기값=0  
-    
-    printf("input a number : ");
-    scanf("%i", &number); //정수를 입력받음  
-    
-    for(i=1; i<=number; i++) //1부터 입력 정수까지 증가  
-    {
-    	sum+=i; // 1부터 입력정수까지 더함  
-    	
-	}
-    printf("the result is %i", sum); //결과값을 출력  
-	return 0;
+    int a, b; // 두 정수 변수 
+    char c;  
+    printf(" 식을 입력하세요: ");
+    scanf(" %i %c %i", &a, &c, &b); //숫자, 연산자, 숫자 입력  
+	
+	if(c== '+') //입력된 문자가 덧셈일때  
+	printf("= %i \n", a+b ); 
+	
+    else if(c=='-') // 입력된 문자가 뺄셈일때  
+	printf("= %i \n", a-b );
+	
+	else if(c=='*') // 입력된 문자가 곱셈일 때  
+	printf("= %i \n", a*b);
+	
+	else if(c=='/') //입력된 문자가 나눗셈일 때  
+	printf("= %i \n", a/b); 
+	
+	else if(c=='%') //입력된 문자가 나머지일 때  
+	printf("= %i \n", a%b);
+	
+return 0; 
   
    }
 	
