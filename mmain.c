87252a122f2fb4a3076e9ffb6296a2633182d234
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-18 11:19:21 +0900

    005실습2

--- mmain.c
+++ mmain.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//5주차 실습 1-양수 음수 판단   
+//5주차 실습 2-절대값 구하기  
 
  
 int main (void)
@@ -12,13 +12,13 @@ int main (void)
    scanf("%i", &i);
     
    if(i>0)
-   printf("양수입니다.");
+   printf("절대값은 %i 입니다.", i);
    
    else if (i<0)
-   printf("음수입니다.");
+   printf("절대값은 %i 입니다.", -i);
    
    else 
-   printf("0입니다."); 
+   printf("절대값은 0입니다."); 
    }
 	
 	 	
