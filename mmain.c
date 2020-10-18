96d3a155dcf195987d9de97e68607339f43832a8
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-18 10:49:56 +0900

    005예제_순환문for

--- mmain.c
+++ mmain.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//5주차 예제- 순환문 do while 문  
- //메뉴선택 프로그램  
+//5주차 예제- 순환문 for  문  
+
  
 int main (void)
 {
-    int i=0; //초기값 설 정  
-    
-    do
-    {
-    	printf("1-새로만들기\n");
-    	printf("2-파일 열기 \n");
-    	printf("3-파일 닫기 \n");
-		printf("하나를 선택하세요. \n");
-		scanf("%i", &i);
-		
-	} while(i<1 || i>3);
+   int i, j;
+   
+   for(i=0; i<5; i++)
+   {
+   	for(j=0; j<10; j++)
+   	{
+   		printf("*");
+	}
+	printf("\n");
+   }
+   
 	
 	 	
 }
