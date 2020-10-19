dfc10ab28c7cab75b1915707b6cd830dfd5df5a3
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-19 13:19:14 +0900

    006실습3

--- mmain.c
+++ mmain.c
@@ -2,20 +2,19 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//6주차 실습1- 함수화 하기 
-void star()
-{   int i;
-	for(i=0; i<10; i++)
-	printf("*  ");
+//6주차 실습3 - call my value  
+
+int square( int a)
+{
+	return(a*a);
+	
 }
 
-int main(void)
+int main()
 {
-	star();
-	star();
-	star();
+	int a =2;
+	a =  square(a);
+	printf("a=%i\n", a);
 	
 	return 0;
-	
-	
-	}	
+}
