cde5267042838384d5b4d68a372763a6ad03e9e7
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-20 03:57:40 +0900

    006실습4

--- mmain.c
+++ mmain.c
@@ -2,19 +2,50 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//6주차 실습3 - call my value  
+//6주차 실습4 - combination 
 
-int square( int a)
+int get_integer()
 {
-	return(a*a);
-	
+
+int x;
+    printf("input an integer: ");
+    scanf("%i", &x);
+    return x; 
 }
 
-int main()
+int factorial(int n)
+{ 	int res, i;
+	res=1;
+	for( i=1; i<=n; i++)
+    res= res*i;
+
+	return res;
+
+}
+
+int combination(int n, int r)
 {
-	int a =2;
-	a =  square(a);
-	printf("a=%i\n", a);
-	
+    int n1, n2, n3;
+    int result;
+    n1 = factorial(n);
+    n2 = factorial(n-r);
+    n3 = factorial(r);
+    
+    result = n1/(n2 * n3);
+    
+    return(n1/n2/n3/result);
+}
+int main (void)
+{
+	int n, r, result;
+
+	n= get_integer();
+
+	r= get_integer();
+
+	result= combination(n,r);
+
+	printf("C(%i, %i) = %i \n", n, r, result);
+
 	return 0;
-}
+}    
