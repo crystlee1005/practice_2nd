c1009c6e6ae1c61d0fc3bb5c00da29e9cd2d4e08
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-20 04:04:20 +0900

    007실습2

--- mmain.c
+++ mmain.c
@@ -2,50 +2,17 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//6주차 실습4 - combination 
+//7주차 실습2
 
-int get_integer()
+int main(void)
 {
-
-int x;
-    printf("input an integer: ");
-    scanf("%i", &x);
-    return x; 
-}
-
-int factorial(int n)
-{ 	int res, i;
-	res=1;
-	for( i=1; i<=n; i++)
-    res= res*i;
-
-	return res;
-
-}
-
-int combination(int n, int r)
-{
-    int n1, n2, n3;
-    int result;
-    n1 = factorial(n);
-    n2 = factorial(n-r);
-    n3 = factorial(r);
-    
-    result = n1/(n2 * n3);
-    
-    return(n1/n2/n3/result);
-}
-int main (void)
-{
-	int n, r, result;
-
-	n= get_integer();
-
-	r= get_integer();
-
-	result= combination(n,r);
-
-	printf("C(%i, %i) = %i \n", n, r, result);
-
+	int i;
+	
+	for (i=0; i<5; i++)
+	{
+		int temp = 1;
+		printf("temp = %d\n", temp);
+		temp++;
+	}
 	return 0;
-}    
+}
